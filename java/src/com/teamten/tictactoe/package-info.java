a5e204506fe84e4a90612a7065e1a5f483234fa6
Lawrence Kesteloot <lk@teamten.com>
2011-01-23 01:57:35 -0500

    Package info for Tic Tac Toe game.

---
+++ java/src/com/teamten/tictactoe/package-info.java
@@ -0,0 +1,10 @@
+
+/**
+ * This is an unfinished experiment to see if the computer can pick moves
+ * in Tic Tac Toe that are most likely to cause human error. Normally a
+ * Tic Tac Toe-playing program will see the entire tree, realize it's
+ * a tie for best play, and play anything. But surely some moves will
+ * have obvious replies and some non-obvious best replies, and this attempts
+ * to figure that out.
+ */
+package com.teamten.tictactoe;
