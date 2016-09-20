699c1e34ad5151ad6e1560c4cc388e0ce6a71424
Lawrence Kesteloot <lk@teamten.com>
2016-09-19 23:59:22 -0700

    Use state machine in Markdown parser; skip comments.

--- java/src/main/java/com/teamten/markdown/MarkdownParser.java
+++ java/src/main/java/com/teamten/markdown/MarkdownParser.java
@@ -1,12 +1,11 @@
 package com.teamten.markdown;
 
-import com.google.common.base.Joiner;
-import com.google.common.base.Splitter;
-import org.apache.commons.io.IOUtils;
-
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,11 +13,11 @@ import java.util.List;
  * Parses a Markdown file into a DOM.
  */
 public class MarkdownParser {
-    private static final Splitter CHUNK_SPLITTER = Splitter.on("\n\n").
-        omitEmptyStrings().trimResults();
-    private static final Splitter LINE_SPLITTER = Splitter.on("\n").
-        omitEmptyStrings().trimResults();
-    private static final Joiner LINE_JOINER = Joiner.on(" ").skipNulls();
+    private enum ParserState {
+        START_OF_LINE,
+        IN_LINE,
+        COMMENT,
+    }
 
     public static void main(String[] args) throws IOException {
         InputStream inputStream = new FileInputStream(args[0]);
@@ -31,43 +30,104 @@ public class MarkdownParser {
     }
 
     public Doc parse(InputStream inputStream) throws IOException {
-        String contents = IOUtils.toString(inputStream, "UTF-8");
+        Reader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
 
         // Our document.
         Doc doc = new Doc();
 
-        // Cut at double newlines.
-        Iterable<String> chunks = CHUNK_SPLITTER.split(contents);
+        ParserState state = ParserState.START_OF_LINE;
+        StringBuilder builder = new StringBuilder();
+        int chOrEof;
+        parsingLoop: while ((chOrEof = reader.read()) != -1) {
+            char ch = (char) chOrEof;
 
-        // Create a block for each.
-        for (String chunk : chunks) {
-            BlockType blockType;
+            /// System.out.printf("%d (%c) %s %s%n", chOrEof, ch, state, builder);
+            switch (state) {
+                case START_OF_LINE:
+                    if (Character.isWhitespace(ch) && ch != '\n') {
+                        // Skip initial white-space.
+                    } else if (ch == '%') {
+                        // Comment, skip rest of line.
+                        state = ParserState.COMMENT;
+                    } else if (ch == '\n') {
+                        // Blank line, end of chunk.
+                        if (builder.length() > 0) {
+                            boolean continueProcessing = emitBlock(doc, builder);
+                            if (!continueProcessing) {
+                                break parsingLoop;
+                            }
+                        }
+                    } else {
+                        builder.append(ch);
+                        state = ParserState.IN_LINE;
+                    }
+                    break;
 
-            // Figure out type of block.
-            if (chunk.startsWith("# ")) {
-                chunk = chunk.substring(1).trim();
-                blockType = BlockType.PART_HEADER;
-            } else if (chunk.startsWith("## ")) {
-                chunk = chunk.substring(2).trim();
-                blockType = BlockType.CHAPTER_HEADER;
-            } else if (chunk.equals("/bye")) {
-                // For testing.
-                break;
-            } else {
-                blockType = BlockType.BODY;
+                case IN_LINE:
+                    if (ch == '\n') {
+                        state = ParserState.START_OF_LINE;
+                        builder.append(' ');
+                    } else {
+                        builder.append(ch);
+                    }
+                    break;
+
+                case COMMENT:
+                    if (ch == '\n') {
+                        // Back to normal.
+                        state = ParserState.START_OF_LINE;
+                    } else {
+                        // Skip comment character.
+                    }
+                    break;
             }
+        }
 
-            // Remove newlines.
-            Iterable<String> lines = LINE_SPLITTER.split(chunk);
-            chunk = LINE_JOINER.join(lines);
-
-            // Create DOM.
-            Span span = new Span(chunk);
-            Block block = new Block(blockType);
-            block.addSpan(span);
-            doc.addBlock(block);
+        // Final block.
+        if (builder.length() > 0) {
+            emitBlock(doc, builder);
         }
 
         return doc;
+    }
+
+    /**
+     * Add the chunk (paragraph) as a block to the document.
+     *
+     * @return whether to continue processing.
+     */
+    private boolean emitBlock(Doc doc, StringBuilder builder) {
+        // Extract the text from the builder.
+        String chunk = builder.toString().trim();
+        builder.setLength(0);
+
+        // Skip empty blocks. These can happen after the file header, etc.
+        if (chunk.isEmpty()) {
+            return true;
+        }
+
+        BlockType blockType;
+
+        // Figure out type of block.
+        if (chunk.startsWith("# ")) {
+            chunk = chunk.substring(1).trim();
+            blockType = BlockType.PART_HEADER;
+        } else if (chunk.startsWith("## ")) {
+            chunk = chunk.substring(2).trim();
+            blockType = BlockType.CHAPTER_HEADER;
+        } else if (chunk.equals("/bye")) {
+            // Terminate processing; for testing.
+            return false;
+        } else {
+            blockType = BlockType.BODY;
+        }
+
+        // Create DOM.
+        Span span = new Span(chunk);
+        Block block = new Block(blockType);
+        block.addSpan(span);
+        doc.addBlock(block);
+
+        return true;
     }
 }
